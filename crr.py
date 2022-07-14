1bc5a4b964a21b29b1efc77383f49b76c4b8da2e
Alexey Bashmakov <alexey.bashmakov@list.ru>
2022-07-14 11:47:04 +0500

    New file: crr.py for calculate CRR

---
+++ crr.py
@@ -0,0 +1,14 @@
+"""
+Построение CRR:
+ - для всего срока и всех врачей
+ - для всего срока и каждого врача
+ - ежегодно для всех врачей
+ - ежегодно для каждого врача
+"""
+
+import datetime as dt
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
